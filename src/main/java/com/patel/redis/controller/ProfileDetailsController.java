29d28a6a1fcdc8e0cab67702bdd280001bdcec5b
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-11-24 14:24:39 +0530

    dev

--- src/main/java/com/patel/redis/controller/ProfileDetailsController.java
+++ src/main/java/com/patel/redis/controller/ProfileDetailsController.java
@@ -26,92 +26,80 @@ import io.swagger.annotations.ApiImplicitParams;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController("v1ProfileDetails")
-@RequestMapping(path = "/v1/profileDetails")
+@RequestMapping(value = "/v1/profileDetails")
 @Slf4j
 public class ProfileDetailsController {
 
-    @Autowired
-    private ProfileDetailsService profileDetailsService;
+	@Autowired
+	private ProfileDetailsService profileDetailsService;
 
-    @PostMapping(path = "/{tenantName}/create", consumes = "application/json",
-                    produces = "application/json")
-    @ApiImplicitParams({@ApiImplicitParam(name = "tenantName", paramType = "path")})
-    public ResponseEntity<?> createProfileDetails(@RequestBody ProfileDetailsCreateRquest request,
-                    @PathVariable(name = "tenantName", required = true) String tenantName)
-                    throws Exception {
-        log.info(":::::Inside ProfileDetailsController Class, createProfileDetais method:::::");
-        try {
-            ProfileDetailsCreateResponse response =
-                            profileDetailsService.createProfileDetails(request, tenantName);
-            return ResponseEntity.status(HttpStatus.CREATED)
-                            .body(new ModelMap().addAttribute("id", response.getId()).addAttribute(
-                                            "msg",
-                                            response.getMsg()));
-        } catch (final TenantNotFoundException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                            .body(new ModelMap().addAttribute("msg", ex.getMessage()));
-        }
-    }
+	@PostMapping(value = "/{tenantName}/create", consumes = "application/json", produces = "application/json")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "tenantName", paramType = "value") })
+	public ResponseEntity<?> createProfileDetails(@RequestBody ProfileDetailsCreateRquest request,
+			@PathVariable(name = "tenantName", required = true) String tenantName) throws Exception {
+		log.info(":::::Inside ProfileDetailsController Class, createProfileDetais method:::::");
+		try {
+			ProfileDetailsCreateResponse response = profileDetailsService.createProfileDetails(request, tenantName);
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(new ModelMap().addAttribute("id", response.getId()).addAttribute("msg", response.getMsg()));
+		} catch (final TenantNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ModelMap().addAttribute("msg", ex.getMessage()));
+		}
+	}
 
-    @GetMapping(path = "/{id}", produces = "application/json")
-    @ApiImplicitParams({@ApiImplicitParam(name = "id", paramType = "path")})
-    public ResponseEntity<?> getProfileDetails(
-                    @PathVariable(name = "id", required = true) Integer id)
-                    throws Exception {
-        try {
-            ProfileDetails profileDetails =
-                            profileDetailsService.getProfileDetailsByUserName(id);
-            return ResponseEntity.status(HttpStatus.OK)
-                            .body(new ModelMap().addAttribute("response", profileDetails));
-        } catch (final ProfileDetailsNotFoundException ex) {
-            return ResponseEntity.status(HttpStatus.OK)
-                            .body(new ModelMap().addAttribute("msg", ex.getMessage()));
-        }
-    }
+	@GetMapping(value = "/{id}", produces = "application/json")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "id", paramType = "value") })
+	public ResponseEntity<?> getProfileDetails(@PathVariable(name = "id", required = true) Integer id)
+			throws Exception {
+		try {
+			ProfileDetails profileDetails = profileDetailsService.getProfileDetailsByUserName(id);
+			return ResponseEntity.status(HttpStatus.OK).body(new ModelMap().addAttribute("response", profileDetails));
+		} catch (final ProfileDetailsNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.OK).body(new ModelMap().addAttribute("msg", ex.getMessage()));
+		}
+	}
 
+	@GetMapping(value = "/{id}/get")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "id", paramType = "value") })
+	public ResponseEntity<?> getAllProfileDetails(@PathVariable(name = "id", required = true) Integer id)
+			throws Exception {
+		try {
+			List<ProfileDetails> listOfProfileDetails = profileDetailsService.getAllProfileDetails(id);
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(new ModelMap().addAttribute("response", listOfProfileDetails));
+		} catch (final TenantNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ModelMap().addAttribute("msg", ex.getMessage()));
+		}
+	}
 
-    @GetMapping(path = "/{id}/get")
-    @ApiImplicitParams({@ApiImplicitParam(name = "id", paramType = "path")})
-    public ResponseEntity<?> getAllProfileDetails(
-                    @PathVariable(name = "id", required = true) Integer id) throws Exception {
-        try {
-            List<ProfileDetails> listOfProfileDetails =
-                            profileDetailsService.getAllProfileDetails(id);
-            return ResponseEntity.status(HttpStatus.OK)
-                            .body(new ModelMap().addAttribute("response", listOfProfileDetails));
-        } catch (final TenantNotFoundException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                            .body(new ModelMap().addAttribute("msg", ex.getMessage()));
-        }
-    }
+	@DeleteMapping(value = "/{email}/{id}")
+	public ResponseEntity<?> deleteProfileDetails(@PathVariable(name = "email", required = true) String email,
+			@PathVariable(name = "id", required = true) Integer id) throws Exception {
+		try {
+			profileDetailsService.deleteProfileDetails(email, id);
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(new ModelMap().addAttribute("msg", "Successfully deleted."));
+		} catch (final ProfileDetailsNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ModelMap().addAttribute("msg", ex.getMessage()));
+		} catch (final TenantNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ModelMap().addAttribute("msg", ex.getMessage()));
+		}
+	}
 
-    @DeleteMapping(path = "/{email}/{id}")
-    public ResponseEntity<?> deleteProfileDetails(
-                    @PathVariable(name = "email", required = true) String email,
-                    @PathVariable(name = "id", required = true) Integer id) throws Exception {
-        try {
-            profileDetailsService.deleteProfileDetails(email, id);
-            return ResponseEntity.status(HttpStatus.OK)
-                            .body(new ModelMap().addAttribute("msg", "Successfully deleted."));
-        } catch (final ProfileDetailsNotFoundException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                            .body(new ModelMap().addAttribute("msg", ex.getMessage()));
-        } catch (final TenantNotFoundException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                            .body(new ModelMap().addAttribute("msg", ex.getMessage()));
-        }
-    }
-
-    @PutMapping(path = "/update", consumes = "application/json")
-    public ResponseEntity<?> updateProfileDetails(@RequestBody ProfileDetailsUpdateRequest payload,
-                    @RequestParam(value = "id", required = true) Integer id) throws Exception {
-        try {
-            profileDetailsService.updateProfileDetails(payload, id);
-            return ResponseEntity.status(HttpStatus.OK)
-                            .body(new ModelMap().addAttribute("msg", "Successfully Updated"));
-        } catch (final ProfileDetailsNotFoundException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                            .body(new ModelMap().addAttribute("msg", ex.getMessage()));
-        }
-    }
+	@PutMapping(value = "/update", consumes = "application/json")
+	public ResponseEntity<?> updateProfileDetails(@RequestBody ProfileDetailsUpdateRequest payload,
+			@RequestParam(value = "id", required = true) Integer id) throws Exception {
+		try {
+			profileDetailsService.updateProfileDetails(payload, id);
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(new ModelMap().addAttribute("msg", "Successfully Updated"));
+		} catch (final ProfileDetailsNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ModelMap().addAttribute("msg", ex.getMessage()));
+		}
+	}
 }
