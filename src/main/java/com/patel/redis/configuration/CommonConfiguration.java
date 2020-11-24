fb26b5882b7a33954148b4d8394d3b616e68f57d
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-11-24 15:42:53 +0530

    refactor Redis Configuration

--- src/main/java/com/patel/redis/configuration/CommonConfiguration.java
+++ src/main/java/com/patel/redis/configuration/CommonConfiguration.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Configuration
+@Configuration("cmnConfiguration")
 public class CommonConfiguration {
-   
-    public BCryptPasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
+
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
